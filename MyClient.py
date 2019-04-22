0342810c5680d565082585bd3f7108a4c69fad3b
Dave McPherson <davemcpherson@wochstudios.com>
2019-04-21 22:51:52 -0400

    using wave libirary to save sound file instead of speech recognition

--- MyClient.py
+++ MyClient.py
@@ -1,3 +1,4 @@
+import wave
 import discord
 import BufSink
 from threading import Thread
@@ -114,34 +115,24 @@ class MyClient(discord.Client):
 
 def poster(bot, buffer, target_channel):
     global close_flag
-
     # we don't want the thread to end, so just loop forever
     while True:
-        # useless to try anything if we don't have anything in the buffer
-        # wait until we have enough data for a 5-second voice clip in the buffer
-        if len(buffer.bytearr_buf) > 960000:
-            # get 5 seconds worth of data from the buffer
-            idx = buffer.bytes_ps * 5
-            slice = buffer.bytearr_buf[:idx]
-
+        if close_flag:
             # if the slice isn't all 0s, create an AudioData instance with it,
             # needed by the speech_recognition lib
-            if any(slice):
-                # trim leading zeroes, should be more accurate
-                idx_strip = slice.index(next(filter(lambda x: x!=0, slice)))
-                if idx_strip:
-                    buffer.freshen(idx_strip)
-                    slice = buffer.bytearr_buf[:idx]
-                # create the AudioData object
-                audio = sr.AudioData(bytes(slice), buffer.sample_rate,
-                    buffer.sample_width)
-
-                with open("recording-results.wav", "wb") as f:
-                    f.write(audio.get_wav_data())
-            # cut the part we just read from the buffer
-            buffer.freshen(idx)
-
-        # since it's an infinite loop, we need some way to break out, once the
-        # program shuts down
-        if close_flag:
+            if len(buffer.bytearr_buf) > 960000:
+                data = buffer.bytearr_buf
+                # if the slice isn't all 0s, create an AudioData instance with it,
+                # needed by the speech_recognition lib
+                if any(data):
+                    # trim leading zeroes, should be more accurate
+                    idx_strip = data.index(next(filter(lambda x: x!=0, data)))
+                    if idx_strip:
+                        buffer.freshen(idx_strip)
+                        data = buffer.bytearr_buf
+                    wavef = wave.open('sound.wav','w')
+                    wavef.setnchannels(1) # stereo
+                    wavef.setsampwidth(buffer.sample_width) 
+                    wavef.setframerate(buffer.sample_rate)
+                    wavef.writeframesraw(bytes(buffer.bytearr_buf))
             break
